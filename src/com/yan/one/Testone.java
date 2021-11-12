ebcb19a6ee69c748ecd1d99ad38559b5958c44bd
yan <yjyxzzz@qq.com>
2021-11-12 19:22:26 +0800

    增加了类Dogo

--- src/com/yan/one/Testone.java
+++ src/com/yan/one/Testone.java
@@ -3,7 +3,7 @@ package com.yan.one;
 public class Testone {
     public static void main(String[] args) {
 
-        for (int i = 0; i <18; i++) {
+        for (int i = 1; i <18; i++) {
             System.out.println("几岁啊脾气这么大:"+i);
         }
 
